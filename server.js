9df3f68bc0b854abc4464aee1c32887bdbc5b074
HeleneBtx <helene.boiteux@ynov.com>
2020-01-12 19:09:14 +0100

    connection bdd

--- server.js
+++ server.js
@@ -4,15 +4,50 @@ let io = require('socket.io').listen(server);
 let ent = require('ent');
 const fs = require('fs');
 
-// let bodyParser = require('body-parser');
-// let path = require('path');
-
 let port = 3000;
 
-//Chargement de la page html 
+// chargement de la page html 
 app.get('/', function (req, res){
     res.sendFile(__dirname + '/index.html');
 });
+
+// connection bdd
+let mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/gantt', {useUnifiedTopology: true,useNewUrlParser: true,})
+.then(() => console.log('DB Connected!'))
+.catch(err => {
+console.log("database connection error");
+});
+
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  // console.log("database connected");
+});
+
+// creation schemas et models
+let tasksSchema = new mongoose.Schema();
+let tasksModel = mongoose.model('gantt_tasks', tasksSchema);
+
+let linksSchema = new mongoose.Schema();
+let linksModel = mongoose.model('gantt_links', linksSchema);
+
+// afficher tout les tasks et links 
+tasksModel.find(null, function (err, tasks) {
+  if (err) { throw err; }
+  // tasks est un tableau de hash
+  console.log('tasks :');
+  console.log(tasks);
+});
+linksModel.find(null, function (err, links) {
+  if (err) { throw err; }
+  // links est un tableau de hash
+  console.log('links :');
+  console.log(links);
+});
+
 
 // connection au serveur central 
 const socket = require('socket.io-client');
